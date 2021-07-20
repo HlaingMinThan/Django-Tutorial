bb260d54dd34168be1ce7273748431360a324c2d
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-07-20 19:43:46 +0630

    django-lesson-8-passing-datas-to-templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from accounts.models import *
 def customers(request):
     return render(request,'accounts/customers.html')
 
 def products(request):
-   return render(request,'accounts/products.html')
+   products=Product.objects.all()
+   return render(request,'accounts/products.html',{
+      'products':products
+   })
 
 def dashboard(request):
-   return render(request,'accounts/dashboard.html')
+   customers=Customer.objects.all()
+   orders=Order.objects.all()
+   total=orders.count();
+   delivered=Order.objects.filter(status="delivered").count()
+   pending=Order.objects.filter(status="pending").count()
+   return render(request,'accounts/dashboard.html',{
+      'customers':customers,
+      'orders':orders,
+      'total':total,
+      'delivered':delivered,
+      'pending':pending
+   })
