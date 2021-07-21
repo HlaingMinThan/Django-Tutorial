f034a51b68969ec9ccc21144866515910de754e0
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-07-21 23:10:02 +0630

    django-lesson-10-named-routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from . import views;
 
 
 urlpatterns = [
-    path('',views.dashboard),
-    path('customers/<int:id>',views.customers),
-    path('products/',views.products),
+    path('',views.dashboard,name='dashboard'),
+    path('customers/<int:id>',views.customers,name='customers.show'),
+    path('products/',views.products,name='products'),
 ]
