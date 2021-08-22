544308a92a9558a878bf3e84d54e0da43a57f235
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-08-22 21:38:44 +0630

    django-lesson-26-build-profile-picture-upload-feature

--- App/settings.py
+++ App/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os;
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,7 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- accounts/models.py
+++ accounts/models.py
@@ -3,13 +3,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Customer(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,null=True)
-    name=models.CharField(max_length=200,null=True)
+    profile_pic=models.ImageField(null=True,blank=True,upload_to="static/profiles")
     email=models.CharField(max_length=200,null=True)
     phone=models.CharField(max_length=200,null=True)
     created_at=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name        
+        return self.user.username        
 
 class Tag(models.Model):
     name=models.CharField(max_length=200,null=True)
