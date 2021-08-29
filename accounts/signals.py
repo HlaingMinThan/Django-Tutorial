a6f8f5ef9f317d1d3139eae8137ef6cfb95a50c6
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-08-29 20:53:13 +0630

    django-lesson-28-what-is-django-signal-101

---
+++ accounts/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User,Group
+from accounts.models import Customer
+
+def create_customer_profile(sender,instance,created,**kwargs):
+    if created :
+         # add customer gp as default
+          gp=Group.objects.get(name="customer")
+          instance.groups.add(gp)
+          #create customer profile for user
+          Customer.objects.create(user=instance)
+
+
+post_save.connect(create_customer_profile,sender=User)
--- accounts/views.py
+++ accounts/views.py
@@ -127,11 +127,6 @@ def register(request):
        form=RegisterForm(request.POST)
        if form.is_valid():
           user=form.save();
-          # add customer gp as default
-          gp=Group.objects.get(name="customer")
-          user.groups.add(gp)
-          #create customer profile for user
-          Customer.objects.create(user=user)
           # login user
           login(request,user)
           return redirect('/')
